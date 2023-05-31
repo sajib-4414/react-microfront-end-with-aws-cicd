94789421b3efb289d0add68ae4f5deed6e5e9513
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2023-05-31 16:14:07 -0600

    removed header file for test

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
-import Header from './components/Header';
+// import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
 
 export default () => {
   return 
   <BrowserRouter>
   <div>
-    <Header/>
+    {/* <Header/> */}
     <MarketingApp/>
   </div>
   </BrowserRouter>
