881b55d225e3749da37f7f15a4116b2f68d92094
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2023-07-11 12:38:35 -0600

    added auth sub app

--- container/src/App.js
+++ container/src/App.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense , useState} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Progress from './components/Progress';
 import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom';
-import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
+
+// Lazily import marketing and auth, means only load/import when we try to show things of these apps on the screen
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co'
+  productionPrefix: 'co',
 });
 
-
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
-  <BrowserRouter>
-  <StylesProvider generateClassName={generateClassName}>
-  <div>
-    <Header/>
-    <MarketingApp/>
-  </div>
-  </StylesProvider>
-  </BrowserRouter>)
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header onSignOut={()=> setIsSignedIn(false) } isSignedIn={isSignedIn} />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={ ()=> setIsSignedIn(true)}/>
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 };
